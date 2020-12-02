0e5ba1d84087159f15bc629959f460180e91155e
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-12-02 09:08:20 -0600

    7x7 grid

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -30,9 +30,6 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 };
 
 HTMLActuator.prototype.restart = function () {
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "restart");
-  }
   this.clearMessage();
 };
 
@@ -116,12 +113,9 @@ HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
 
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "end", type, this.score);
-  }
-
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+};
 
 HTMLActuator.prototype.clearMessage = function () {
   this.messageContainer.classList.remove("game-won", "game-over");
